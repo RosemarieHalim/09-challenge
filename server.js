b2380e0c4403fb283f31dc951d8af90e21669794
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-08 00:21:40 -0400

    added post, delete, and listen to server.js

--- server.js
+++ server.js
@@ -18,11 +18,27 @@ app.get('*', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
- 
+  let note = req.body;
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteLength = (noteList.length).toString();
+
+  note.id = noteLength;
+  noteList.push(note);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  res.json(noteList);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteID = (req.params.id).toString();
+
+  noteList = noteList.filter(selected => {
+    return selected.id != noteID;
+  })
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  res.json(noteList);
 });
 
 app.listen(PORT, () => {
