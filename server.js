c543802958f20632303b8d7ede5e327efc82310f
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-08 21:10:56 -0400

    fixed missing list of notes

--- server.js
+++ server.js
@@ -10,16 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.post('/api/notes', (req, res) => {
   let note = req.body;
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteList = JSON.parse(fs.readFileSync('/db/db.json', 'utf-8'));
   let noteLength = (noteList.length).toString();
 
   note.id = noteLength;
@@ -30,14 +34,14 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteList = JSON.parse(fs.readFileSync('/db/db.json', 'utf-8'));
   let noteID = (req.params.id).toString();
 
   noteList = noteList.filter(selected => {
     return selected.id != noteID;
   })
 
-  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  fs.writeFileSync('/db/db.json', JSON.stringify(noteList));
   res.json(noteList);
 });
 
