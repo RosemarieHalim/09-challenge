6dfdb4daf963b4da9cecdeafb1a579ca55df85e6
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-08 20:09:10 -0400

    removing new code

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,10 +9,36 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/noteRoute')(app);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
 
-app.use((req, res) => {
-  res.status(404).end();
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.post('/api/notes', (req, res) => {
+  let note = req.body;
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteLength = (noteList.length).toString();
+
+  note.id = noteLength;
+  noteList.push(note);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  res.json(noteList);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  let noteID = (req.params.id).toString();
+
+  noteList = noteList.filter(selected => {
+    return selected.id != noteID;
+  })
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+  res.json(noteList);
 });
 
 app.listen(PORT, () => {
